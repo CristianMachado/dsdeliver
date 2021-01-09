974fdab4d7de3e735dcaf00902adef49699a0be8
Cristian de Almeida Machado <cris02111996@gmail.com>
2021-01-08 21:16:17 -0300

    Search order location

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -1,11 +1,18 @@
 import { type } from "os";
 
 export type Product = {
- 
-    id: number; 
+
+    id: number;
     name: string;
     price: number
     description: string;
     imageUri: string;
 
+}
+
+export type OrderLocationdata = {
+
+    latitude: number;
+    longitude: number;
+    address: string;
 }
